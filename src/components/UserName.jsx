a70d1c4cba303ca4565a515e80cdaf19feaf056f
DebasishKisan1 <debasish.kisan@gmail.com>
2024-01-16 23:21:20 +0530

    refs

--- src/components/UserName.jsx
+++ src/components/UserName.jsx
@@ -3,7 +3,7 @@ import './Username.css'
 const Username = (props)=>{
     return(
         <div className="name">
-            {props.data.name + ' ( ' + props.data.age + ' years old )'}
+            {props.data.name + ' ( ' + props.data.age + ' years old )' + props.data.colg}
         </div>
     )
 }
--- src/components/Userform.jsx
+++ src/components/Userform.jsx
@@ -1,47 +1,38 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 import './Userform.css'
 
 const Userform = (props) => {
-    const [uname, setUname] = useState('')
-    const [uage, setUage] = useState('')
-    const nameHandler = (event) => {
-        setUname((prev) => {
-            return event.target.value
-        })
-    }
-    const ageHandler = (event) => {
-        setUage((prev) => {
-            return event.target.value
-        })
-    }
+    const uname = useRef();
+    const uage = useRef();
+    const ucolg = useRef();
+
     const formHandler = (event) => {
         event.preventDefault();
-        if (uname.length === 0 || uage.length === 0) {
+        if (uname.current.value.length === 0 || uage.current.value.length === 0 || ucolg.current.value.length === 0) {
             return props.isinvalidset('Please enter a valid name and age (non-empty values)')
         }
-        if (uage < 1) {
+        if (uage.current.value < 1) {
             return props.isinvalidset('Please Enter a Valid Age (>0)')
         }
         const u = {
             id: Math.random(),
-            name: uname,
-            age: uage
+            name: uname.current.value,
+            age: uage.current.value,
+            colg: ucolg.current.value
         }
         props.addUser(u)
-        setUname((prev) => {
-            return ''
-        })
-        setUage((prev) => {
-            return ''
-        })
-
+        uname.current.value = ""
+        uage.current.value = ""
+        ucolg.current.value = ""
     }
     return (
         <form onSubmit={formHandler}>
             <label>Username</label>
-            <input value={uname} type="text" onChange={nameHandler} />
+            <input type="text" ref={uname} />
             <label>Age (Years)</label>
-            <input value={uage} type="number" onChange={ageHandler} />
+            <input type="number" ref={uage} />
+            <label>College Name</label>
+            <input type="text" ref={ucolg} />
             <button type="submit">Add User</button>
         </form>
     )
